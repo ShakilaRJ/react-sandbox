13cc621b206c55128b76e3f860c20ba34b81c725
ShakilaRJ <shakilaruwanj@gmail.com>
2026-03-19 14:42:08 +0200

    Week1 - Updated team names

--- week1/src/App.tsx
+++ week1/src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="p-8 bg-slate-50 min-h-screen flex flex-col items-center">
       <h1 className="text-2xl font-bold mb-8 text-slate-700">Welcome to React!</h1>
-      <Profile name="Shakila Ruwan" role="your role" />
+      <Profile name="Shakila Ruwan" role="FRONTEND" />
       <div className="flex gap-4 mt-8">
         <TeamCard name="Shakila" role="FRONTEND" />
         <TeamCard name="Hidula" role="BACKEND" />
