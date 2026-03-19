875f44b5507550cbc2486d23f67d0923b4f719c2
ShakilaRJ <shakilaruwanj@gmail.com>
2026-03-19 14:38:36 +0200

    Week1 - Updated team names

--- week1/src/App.tsx
+++ week1/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Profile } from './components/Profile';
 import { TeamCard } from './components/TeamCard';
 
 export default function App() {
   return (
     <div className="p-8 bg-slate-50 min-h-screen flex flex-col items-center">
       <h1 className="text-2xl font-bold mb-8 text-slate-700">Welcome to React!</h1>
-      <div className="flex gap-4">
-        <TeamCard name="Shakila Jayathilaka" role="Full Stack Developer" />
-        <TeamCard name="Team Member 2" role="UI Designer" />
-        <TeamCard name="Team Member 3" role="Backend Developer" />
+      <Profile name="Shakila Ruwan" role="your role" />
+      <div className="flex gap-4 mt-8">
+        <TeamCard name="Shakila" role="DEVELOPER" />
+        <TeamCard name="Hidula" role="DEVELOPER" />
+        <TeamCard name="Madushanka" role="DEVELOPER" />
       </div>
     </div>
   );
